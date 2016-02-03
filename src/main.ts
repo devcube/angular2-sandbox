a98d32e4c599eade8249ecafdf575b74a79bb526
Mike South <micke@devcube.se>
2016-02-03 22:42:14 +0100

    Merged changes from angular2-webpack-starter up until 14a3a34, Feb 3, 2016 8:13 PM

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,3 @@
-/*
- * Providers provided by Angular
- */
 import {provide, enableProdMode} from 'angular2/core';
 import {bootstrap, ELEMENT_PROBE_PROVIDERS} from 'angular2/platform/browser';
 import {ROUTER_PROVIDERS, LocationStrategy, HashLocationStrategy} from 'angular2/router';
@@ -10,20 +7,12 @@ const ENV_PROVIDERS = [];
 
 if ('production' === process.env.ENV) {
   enableProdMode();
-// } else {
+} else {
+   ENV_PROVIDERS.push(ELEMENT_PROBE_PROVIDERS);
 }
-  ENV_PROVIDERS.push(ELEMENT_PROBE_PROVIDERS);
 
-/*
- * App Component
- * our top level component that holds all of our components
- */
 import {App} from './app/app';
 
-/*
- * Bootstrap our Angular app with a top level component `App` and inject
- * our Services and Providers into Angular's dependency injection
- */
 document.addEventListener('DOMContentLoaded', function main() {
   bootstrap(App, [
     ...ENV_PROVIDERS,
